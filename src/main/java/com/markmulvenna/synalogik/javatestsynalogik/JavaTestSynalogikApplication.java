9a5f62b0302313e974d974a6c23f82c3a416d386
MarkMulvenna <73496414+MarkMulvenna@users.noreply.github.com>
2022-03-10 16:52:04 +0000

    Initial Configuration of Spring, no applicable use right now but the barebone foundations.

--- src/main/java/com/markmulvenna/synalogik/javatestsynalogik/JavaTestSynalogikApplication.java
+++ src/main/java/com/markmulvenna/synalogik/javatestsynalogik/JavaTestSynalogikApplication.java
@@ -2,12 +2,21 @@ package com.markmulvenna.synalogik.javatestsynalogik;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JavaTestSynalogikApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JavaTestSynalogikApplication.class, args);
+    }
+
+    @GetMapping("/word-counter")
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+       return String.format("Hello %s!", name);
     }
 
 }
