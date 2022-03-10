4d1c62554484e3545475dac41de338b8e43e69b5
MarkMulvenna <73496414+MarkMulvenna@users.noreply.github.com>
2022-03-10 16:24:53 +0000

    Initial commit

---
+++ src/main/java/com/markmulvenna/synalogik/javatestsynalogik/JavaTestSynalogikApplication.java
@@ -0,0 +1,13 @@
+package com.markmulvenna.synalogik.javatestsynalogik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTestSynalogikApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTestSynalogikApplication.class, args);
+    }
+
+}
